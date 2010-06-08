550130114f8455d1ae0949dcd149fcfd57b22576
Ramesh <ramesh373@hotmail.co.uk>
2010-06-08 12:33:21 +0100

    Adding config folder

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'ee7166f69e56481e9f9c7b444d69534bd1e0658cf197975827e9a98fe7d05d2ae884bee1fb504bcaa6e594ad4af52b8cc17574bd2623cca7766825e2c7df4c55';
